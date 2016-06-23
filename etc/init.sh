19f68b547fdb411947f8b591568aaa9dabab302a
Todd Volkert <tvolkert@google.com>
2016-06-23 14:33:01 -0700

    Made code more forgiving of the current project being unset

--- etc/init.sh
+++ etc/init.sh
@@ -4,4 +4,7 @@
 
 $PROJ_BIN/update_current_if_needed || exit $?
 export CURRENT=$($PROJ_BIN/get_current) || die "could not get current project"
-cd $PROJ_BASE/all/$CURRENT/home || die "could not open project home"
+[[ -n "$CURRENT" ]] || die "current project is unset"
+home_symlink=$PROJ_BASE/all/$CURRENT/home
+[[ -h $home_symlink ]] || die "home symlink missing for project: ~$CURRENT/home"
+cd $home_symlink&>/dev/null || die "could not open project home"
